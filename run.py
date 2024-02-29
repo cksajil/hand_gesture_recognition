f77c9adcab543c6cbab60c7000ca0e6928001f37
cksajil <sajilck@gmail.com>
2024-02-29 21:48:47 +0530

    update main file

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 # https://developers.google.com/mediapipe/solutions/vision/gesture_recognizer
 import cv2
+import time
+import streamlit as st
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
@@ -17,20 +19,44 @@ CLASSES = [
     "Victory",
     "ILoveYou",
 ]
-cv2.namedWindow("preview")
-cap = cv2.VideoCapture(0)
-width = 640
-height = 480
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-while True:
-    success, raw_frame = cap.read()
+
+def predict_frame(raw_frame):
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=raw_frame)
     recognition_result = recognizer.recognize(mp_image)
     top_gesture = recognition_result.gestures
+    gesture_detected = "None"
     if top_gesture:
-        hand_landmarks = recognition_result.hand_landmarks
-        print(top_gesture[0][0].category_name)
-    cv2.imshow("Image", raw_frame)
-    cv2.waitKey(1)
+        gesture_detected = top_gesture[0][0].category_name
+    return gesture_detected
+
+
+def main():
+    st.title("Gesture Controlled Infographics")
+    cap = cv2.VideoCapture(0)
+    width = 128
+    height = 128
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    frame_placeholder = st.empty()
+    class_placeholder = st.empty()
+    stop_button_pressed = st.button("Stop")
+    while cap.isOpened() and not stop_button_pressed:
+        success, raw_frame = cap.read()
+        # time.sleep(1)
+        gesture_detected = predict_frame(raw_frame)
+        st.write(gesture_detected)
+        if not success:
+            st.write("Video Capture Ended")
+            break
+        raw_frame = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2RGB)
+        frame_placeholder.image(raw_frame, channels="RGB")
+        class_placeholder.header(gesture_detected)
+        if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
