9ba425b3a843207c73c9d09b89f417419fd22c92
cksajil <sajilck@gmail.com>
2024-03-17 21:45:24 +0530

    update GPIO utility funcs

--- utils.py
+++ utils.py
@@ -1,4 +1,23 @@
+# Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
+
+
 def read_markdown_file(markdown_path):
     with open(markdown_path, "r") as file:
         markdown_content = file.read()
     return markdown_content
+
+
+def setup_gpio():
+    GPIO.setwarnings(False)  # Ignore warning for now
+    GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+
+    for pin in range(11, 17):
+        GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+        # Set pin to be an output pin and set initial value to low (off)
+
+
+def gpio_action(code):
+    pins = list(range(range(11, 17)))
+    for pin, value in zip(pins, code):
+        GPIO.output(pin, value)
