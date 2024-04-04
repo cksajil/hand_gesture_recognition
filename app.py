4a08f485875bbe072f4c70afceae598e2fc8f451
cksajil <sajilck@gmail.com>
2024-04-04 20:51:52 +0530

    update response delay using logic

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from mediapipe.tasks.python import vision
 base_options = python.BaseOptions(model_asset_path="./models/gesture_recognizer.task")
 options = vision.GestureRecognizerOptions(base_options=base_options)
 recognizer = vision.GestureRecognizer.create_from_options(options)
+DELAY_COUNT = 10
+SELECTED_CLASSES = ["Thumb_Up", "Thumb_Down"]
 CLASSES = [
     "None",
     "Closed_Fist",
@@ -34,20 +36,9 @@ def load_image(file):
     return cv2.imread(file)
 
 
-def main_page(
-    frame_holder, class_holder, html_holder, gesture_detected, idx
-):
-    if gesture_detected in ["Thumb_Up", "Thumb_Down"]:
-        if gesture_detected == "Thumb_Up":
-            idx += 1
-        else:
-            idx -= 1
-    else:
-        pass
-    idx = idx % 6
+def main_page(html_holder, idx):
     current_page = pages[idx]
     html_holder.image(load_image(join("static", current_page)), channels="BGR")
-    class_holder.write(gesture_detected)
     return idx
 
 
@@ -62,16 +53,15 @@ def predict_frame(raw_frame):
 
 
 def main():
-    cap = cv2.VideoCapture(-1)
+    cap = cv2.VideoCapture(0)
     width = 16
     height = 16
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     stop_button_pressed = st.button("Stop")
-    frame_holder = st.empty()
     html_holder = st.empty()
-    class_holder = st.empty()
     idx = 0
+    gesture_buffer = []
     while cap.isOpened() and not stop_button_pressed:
         success, raw_frame = cap.read()
         if not success:
@@ -79,9 +69,21 @@ def main():
             break
         raw_frame = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2RGB)
         gesture_detected = predict_frame(raw_frame)
-        idx = main_page(
-            frame_holder, class_holder, html_holder, gesture_detected, idx
-        )
+        if gesture_detected not in SELECTED_CLASSES:
+            continue
+        else:
+            gesture_buffer.append(gesture_detected)
+            gesture_buffer = gesture_buffer[-DELAY_COUNT:]
+            up_count = gesture_buffer.count("Thumb_Up")
+            down_count = gesture_buffer.count("Thumb_Down")
+            if up_count == DELAY_COUNT:
+                idx += 1
+                gesture_buffer.clear()
+            elif down_count == DELAY_COUNT:
+                idx -= 1
+                gesture_buffer.clear()
+            idx = idx % 6
+            idx = main_page(html_holder, idx)
         if stop_button_pressed:
             break
     cap.release()
