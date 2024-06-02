7ad7929bf1ac7e87856fff882d7da41689a84e5c
cksajil <sajilck@gmail.com>
2024-06-02 18:11:18 +0530

    MVP of dynamic gesture control

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
+import os
 import cv2
+import json
+import torch
+import numpy as np
+from PIL import Image
 import streamlit as st
-import mediapipe as mp
 from os.path import join
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+from collections import OrderedDict
+from torchvision.transforms import *
+from utils import load_config, ConvColumn
 from utils import read_html_file, generate_gif_content
 from utils import setup_gpio, gpio_action, gpio_clear
 
-
-base_options = python.BaseOptions(model_asset_path="./models/gesture_recognizer.task")
-options = vision.GestureRecognizerOptions(base_options=base_options)
-recognizer = vision.GestureRecognizer.create_from_options(options)
-DELAY_COUNT = 10
 NUM_PAGES = 8
-SELECTED_CLASSES = ["Thumb_Up", "Thumb_Down"]
-CLASSES = [
-    "None",
-    "Closed_Fist",
-    "Open_Palm",
-    "Pointing_Up",
-    "Thumb_Down",
-    "Thumb_Up",
-    "Victory",
-    "ILoveYou",
-]
-
+SELECTED_CLASSES = ["Slide Two Fingers Left", "Slide Two Fingers Right"]
+CLASSES = {
+    0: "No Gesture",
+    1: "Slide Two Fingers Left",
+    2: "Slide Two Fingers Right",
+    3: "Slide Two Fingers Down",
+    4: "Slide Two Fingers Up",
+    5: "Shaking Hand",
+    6: "Stop Sign",
+    7: "Pull Two Fingers In",
+}
 pages = [
     "cpu.html",
     "network_card.html",
@@ -68,18 +67,99 @@ def predict_frame(raw_frame):
     return gesture_detected
 
 
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.cpu().topk(maxk, 1, True, True)
+    top_pred = pred[0][0]
+
+    gesture_detected = CLASSES[top_pred.item()]
+
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    gesture_label_int = top_pred.item()
+    return gesture_label_int, gesture_detected
+
+
+def get_frame_names(frames):
+    nclips = 1
+    is_val = False
+    step_size = 2
+    clip_size = 18
+    frame_names = frames
+    num_frames = len(frames)
+
+    # set number of necessary frames
+    if nclips > -1:
+        num_frames_necessary = clip_size * nclips * step_size
+    else:
+        num_frames_necessary = num_frames
+
+    # pick frames
+    offset = 0
+    if num_frames_necessary > num_frames:
+        # pad last frame if video is shorter than necessary
+        frame_names += [frame_names[-1]] * (num_frames_necessary - num_frames)
+    elif num_frames_necessary < num_frames:
+        # If there are more frames, then sample starting offset
+        diff = num_frames - num_frames_necessary
+        # Temporal augmentation
+        if not is_val:
+            offset = np.random.randint(0, diff)
+    frame_names = frame_names[offset : num_frames_necessary + offset : step_size]
+    return frame_names
+
+
 def main():
+    device = torch.device("cpu")
+    config = load_config("config.json")
     setup_gpio()
     cap = cv2.VideoCapture(0)
-    width = 16
-    height = 16
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    width = 720
+    height = 1280
     stop_button_pressed = st.button("Stop")
     gif_holder = st.empty()
     html_holder = st.empty()
     idx = 0
     gesture_buffer = []
+    n = 0
+    frames = np.empty((0, 100, 176, 3))
+
+    transform = Compose(
+        [
+            CenterCrop(84),
+            ToTensor(),
+            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
+    model = ConvColumn(8)
+
+    if os.path.isfile(config["checkpoint"]):
+        print("loading checkpoint")
+        checkpoint = torch.load(config["checkpoint"], map_location="cpu")
+        new_state_dict = OrderedDict()
+
+        for k, v in checkpoint.items():
+            if k == "state_dict":
+                del checkpoint["state_dict"]
+                for j, val in v.items():
+                    name = j[7:]
+                    new_state_dict[name] = val
+                checkpoint["state_dict"] = new_state_dict
+                break
+        model.load_state_dict(checkpoint["state_dict"])
+        print("loaded checkpoint")
+    else:
+        print("no checkpoint found at '{}'".format(config["checkpoint"]))
+
     while not stop_button_pressed:
         while cap.isOpened() and not stop_button_pressed:
             success, raw_frame = cap.read()
@@ -87,20 +167,36 @@ def main():
                 st.write("Video Capture Ended")
                 break
             raw_frame = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2RGB)
-            gesture_detected = predict_frame(raw_frame)
-            if gesture_detected not in SELECTED_CLASSES:
-                continue
-            else:
-                gesture_buffer.append(gesture_detected)
-                gesture_buffer = gesture_buffer[-DELAY_COUNT:]
-                up_count = gesture_buffer.count("Thumb_Up")
-                down_count = gesture_buffer.count("Thumb_Down")
-                if up_count == DELAY_COUNT:
+            raw_frame = cv2.resize(raw_frame, (176, 100))
+            frames = np.append(frames, [raw_frame], axis=0)
+            n += 1
+            if n == 37:
+                imgs = []
+                frames = get_frame_names(frames)
+                for frame in frames:
+                    frame = Image.fromarray((frame * 255).astype(np.uint8))
+                    frame = transform(frame)
+                    imgs.append(torch.unsqueeze(frame, 0))
+
+                data = torch.cat(imgs)
+                data = data.permute(1, 0, 2, 3)
+                data = data[None, :, :, :, :]
+                target = [2]
+                target = torch.tensor(target)
+                data = data.to(device)
+
+                model.eval()
+                output = model(data)
+
+                gesture_label_int, gesture_detected = accuracy(
+                    output.detach(), target.detach().cpu(), topk=(1, 5)
+                )
+                n = 0
+                frames = frames = np.empty((0, 100, 176, 3))
+                if gesture_label_int == 2:
                     idx += 1
-                    gesture_buffer.clear()
-                elif down_count == DELAY_COUNT:
+                elif gesture_label_int == 1:
                     idx -= 1
-                    gesture_buffer.clear()
                 idx = idx % NUM_PAGES
                 idx = main_page(gif_holder, html_holder, idx)
                 gpio_action(idx)
--- utils.py
+++ utils.py
@@ -1,9 +1,47 @@
-# Import Raspberry Pi GPIO library
+import json
+import torch
 import base64
+import torch.nn as nn
 from os.path import join
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 led_map = {0: 7, 1: 11, 2: 13, 3: 15, 4: 12, 5: 16, 6: 18, 7: 22}
+
+class ConvColumn(nn.Module):
+
+    def __init__(self, num_classes):
+        super(ConvColumn, self).__init__()
+        self.conv_layer1 = self._make_conv_layer(3, 64, (1, 2, 2), (1, 2, 2))
+        self.conv_layer2 = self._make_conv_layer(64, 128, (2, 2, 2), (2, 2, 2))
+        self.conv_layer3 = self._make_conv_layer(128, 256, (2, 2, 2), (2, 2, 2))
+        self.conv_layer4 = self._make_conv_layer(256, 256, (2, 2, 2), (2, 2, 2))
+
+        self.fc5 = nn.Linear(12800, 512)
+        self.fc5_act = nn.ELU()
+        self.fc6 = nn.Linear(512, num_classes)
+
+    def _make_conv_layer(self, in_c, out_c, pool_size, stride):
+        conv_layer = nn.Sequential(
+            nn.Conv3d(in_c, out_c, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm3d(out_c),
+            nn.ELU(),
+            nn.MaxPool3d(pool_size, stride=stride, padding=0),
+        )
+        return conv_layer
+
+    def forward(self, x):
+        x = self.conv_layer1(x)
+        x = self.conv_layer2(x)
+        x = self.conv_layer3(x)
+        x = self.conv_layer4(x)
+
+        x = x.view(x.size(0), -1)
+
+        x = self.fc5(x)
+        x = self.fc5_act(x)
+
+        x = self.fc6(x)
+        return x
 
 
 def read_html_file(file_path):
@@ -27,23 +65,30 @@ def generate_gif_content(file_name):
 
 
 def setup_gpio():
-    GPIO.setwarnings(False)  # Ignore warning for now
-    GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-    for key in led_map:
-        GPIO.setup(led_map[key], GPIO.OUT, initial=GPIO.LOW)
+    pass
+    # GPIO.setwarnings(False)  # Ignore warning for now
+    # GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+    # for key in led_map:
+    #     GPIO.setup(led_map[key], GPIO.OUT, initial=GPIO.LOW)
 
 
 def gpio_clear():
     for key in led_map:
         print("Pin number {} is OFF".format(led_map[key]))
-        GPIO.output(led_map[key], GPIO.LOW)
+        # GPIO.output(led_map[key], GPIO.LOW)
 
 
 def gpio_action(pin):
     for key in led_map:
         if key == pin:
             print("Pin number {} is ON".format(led_map[key]))
-            GPIO.output(led_map[key], GPIO.HIGH)
+            # GPIO.output(led_map[key], GPIO.HIGH)
         else:
             print("Pin number {} is OFF".format(led_map[key]))
-            GPIO.output(led_map[key], GPIO.LOW)
+            # GPIO.output(led_map[key], GPIO.LOW)
+
+
+def load_config(config_name, CONFIG_PATH="./config"):        
+    with open(join(CONFIG_PATH, config_name)) as file:
+        config = json.load(file)
+    return config
