20b607b8ce293f353d6b4a3972cc0f403737e518
cksajil <sajilck@gmail.com>
2024-06-04 07:01:33 +0530

    supress gpio

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import torch
 import base64
 import torch.nn as nn
 from os.path import join
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 led_map = {0: 7, 1: 11, 2: 13, 3: 15, 4: 12, 5: 16, 6: 18, 7: 22}
 
@@ -56,26 +56,27 @@ def read_html_file(file_path):
 
 
 def setup_gpio():
-    GPIO.setwarnings(False)  # Ignore warning for now
-    GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-    for key in led_map:
-        GPIO.setup(led_map[key], GPIO.OUT, initial=GPIO.LOW)
+    pass
+    # GPIO.setwarnings(False)  # Ignore warning for now
+    # GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+    # for key in led_map:
+    #     GPIO.setup(led_map[key], GPIO.OUT, initial=GPIO.LOW)
 
 
 def gpio_clear():
     for key in led_map:
-        # print("Pin number {} is OFF".format(led_map[key]))
-        GPIO.output(led_map[key], GPIO.LOW)
+        print("Pin number {} is OFF".format(led_map[key]))
+        # GPIO.output(led_map[key], GPIO.LOW)
 
 
 def gpio_action(pin):
     for key in led_map:
         if key == pin:
-            # print("Pin number {} is ON".format(led_map[key]))
-            GPIO.output(led_map[key], GPIO.HIGH)
+            print("Pin number {} is ON".format(led_map[key]))
+            # GPIO.output(led_map[key], GPIO.HIGH)
         else:
-            # print("Pin number {} is OFF".format(led_map[key]))
-            GPIO.output(led_map[key], GPIO.LOW)
+            print("Pin number {} is OFF".format(led_map[key]))
+            # GPIO.output(led_map[key], GPIO.LOW)
 
 
 def load_config(config_name, CONFIG_PATH="./config"):
