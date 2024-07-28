66acf04c35b659fd9b90bba3bd8005936e91d3be
sajilc <sajilck@gmail.com>
2024-07-28 14:32:22 +0530

    update script

--- app.py
+++ app.py
@@ -151,9 +151,10 @@ def process_video_stream(model, device, transform):
                 output.detach(), target.detach().cpu(), topk=(1,)
             )
             gesture_buffer.append(gesture_label_int)
+            print(gesture_buffer)
             gesture_buffer = gesture_buffer[-30:]
             no_action_count = gesture_buffer.count(0)
-            print("no action count:", no_action_count)
+            # print("no action count:", no_action_count)
             n = 0
             frames = np.empty((0, 100, 176, 3))
             if no_action_count > DELAY_COUNT:
