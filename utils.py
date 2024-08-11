c96f2bfa0f5524dff7746f31a80ac743ddcdcb3e
sajilc <sajilck@gmail.com>
2024-08-11 14:46:07 +0530

    tweak params

--- utils.py
+++ utils.py
@@ -102,7 +102,7 @@ def capture_image():
                 "-r",
                 "176x100",
                 "--jpeg",
-                "200",
+                "15s0",
                 image_filename,
             ],
             stdout=devnull,
