fdb40a459ee8e026721fc5943455afe45a609113
sajilc <sajilck@gmail.com>
2024-08-11 14:54:46 +0530

    added noflask

--- app.py
+++ app.py
@@ -2,19 +2,13 @@ import os
 import cv2
 import time
 import torch
-import logging
-import socket
 import numpy as np
 from PIL import Image
 import torch.nn as nn
-from os.path import join
 from threading import Thread
 from collections import OrderedDict
-from flask_socketio import SocketIO, emit
-from flask import Flask, render_template_string
 from torchvision.transforms import Compose, CenterCrop, Normalize, ToTensor
-from utils import load_config, ConvColumn, setup_gpio, gpio_action, read_html_file
-from utils import capture_image
+from utils import load_config, ConvColumn, setup_gpio, gpio_action, capture_image
 
 NUM_PAGES = 9
 SELECTED_CLASSES = ["Slide Two Fingers Left", "Slide Two Fingers Right"]
@@ -28,22 +22,19 @@ CLASSES = {
     6: "Stop Sign",
     7: "Pull Two Fingers In",
 }
+
 pages = [
-    "home.html",
-    "cpu.html",
-    "network_card.html",
-    "smps.html",
-    "motherboard.html",
-    "gpu.html",
-    "fan.html",
-    "storage.html",
-    "ram.html",
+    "action.gif",
+    "cpu.gif",
+    "network_card.gif",
+    "smps.gif",
+    "motherboard.gif",
+    "gpu.gif",
+    "fan.gif",
+    "storage.gif",
+    "ram.gif",
 ]
 
-app = Flask(__name__)
-log = logging.getLogger("werkzeug")
-log.disabled = True
-socketio = SocketIO(app)
 current_page = {"page": pages[0]}
 
 
@@ -107,11 +98,18 @@ def load_model(config_path):
     return model
 
 
-@app.route("/page_content")
-def page_content():
-    page = current_page["page"]
-    page_html = read_html_file(join("static", page))
-    return render_template_string(page_html)
+def display_gif(gif_path):
+    cap = cv2.VideoCapture(gif_path)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            continue
+        cv2.imshow("GIF Display", frame)
+        if cv2.waitKey(50) & 0xFF == ord("q"):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 def process_video_stream(model, device, transform):
@@ -169,43 +167,11 @@ def process_video_stream(model, device, transform):
                     start_time = time.time()
 
             idx = idx % NUM_PAGES
-            page = pages[idx]
-            current_page["page"] = page
+            gif_path = pages[idx]
+            current_page["page"] = gif_path
 
             gpio_action(idx)
-            socketio.emit("page_change", {"page": page})
-
-
-@app.route("/")
-def index():
-    page = current_page["page"]
-    page_html = read_html_file(join("static", page))
-    return render_template_string(
-        """
-        {{ page_html|safe }}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.min.js"></script>
-        <script type="text/javascript">
-            var socket = io();
-            socket.on('connect', function() {
-                console.log('Connected to server');
-            });
-            socket.on('page_change', function(data) {
-                console.log('Page change to: ' + data.page);
-                fetch('/page_content')
-                    .then(response => response.text())
-                    .then(html => {
-                        document.body.innerHTML = html;
-                    });
-            });
-        </script>
-    """
-    )
-
-
-@socketio.on("connect")
-def handle_connect():
-    page = current_page["page"]
-    emit("page_change", {"page": page})
+            display_gif(gif_path)
 
 
 if __name__ == "__main__":
@@ -213,7 +179,6 @@ if __name__ == "__main__":
     model = load_model("config.json")
     model.eval()
     model = torch.quantization.quantize_dynamic(model, {nn.Linear}, dtype=torch.qint8)
-    # model = torch.jit.script(model)
 
     device = torch.device("cpu")
     transform = Compose(
@@ -230,4 +195,5 @@ if __name__ == "__main__":
     # Print the IP address
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
-    socketio.run(app, host="0.0.0.0", port=5001, debug=True)
+    print(f"Server running on IP: {ip_address}")
+    print("Press 'q' to quit.")
