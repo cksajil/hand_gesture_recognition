f44bb28d6a18cc42f3186815138de9e9b1844fcb
sajilc <sajilck@gmail.com>
2024-09-25 20:43:28 +0530

    Adding autopilot code

--- app.py
+++ app.py
@@ -1,34 +1,19 @@
 import os
-import cv2
 import time
-import torch
 import logging
 import socket
 import numpy as np
-from PIL import Image
-import torch.nn as nn
 from os.path import join
 from threading import Thread
-from collections import OrderedDict
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template_string
-from torchvision.transforms import Compose, CenterCrop, Normalize, ToTensor
-from utils import load_config, ConvColumn, read_html_file, find_arduino_port
-from utils import capture_image, communicate_with_arduino
+from utils import communicate_with_arduino
+from utils import load_config, read_html_file, find_arduino_port
 
-DELAY_COUNT = 5
+
 NUM_PAGES = 9
-SELECTED_CLASSES = ["Slide Two Fingers Left", "Slide Two Fingers Right"]
-CLASSES = {
-    0: "No Gesture",
-    1: "Slide Two Fingers Left",
-    2: "Slide Two Fingers Right",
-    3: "Slide Two Fingers Down",
-    4: "Slide Two Fingers Up",
-    5: "Shaking Hand",
-    6: "Stop Sign",
-    7: "Pull Two Fingers In",
-}
+SWITCHING_DELAY = 20
+
 pages = [
     "home.html",
     "cpu.html",
@@ -48,66 +33,6 @@ socketio = SocketIO(app)
 current_page = {"page": pages[0]}
 
 
-def accuracy(output, target, topk=(1,)):
-    maxk = max(topk)
-    batch_size = target.size(0)
-    _, pred = output.cpu().topk(maxk, 1, True, True)
-    top_pred = pred[0][0]
-    gesture_detected = CLASSES[top_pred.item()]
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-    res = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
-        res.append(correct_k.mul_(100.0 / batch_size))
-    gesture_label_int = top_pred.item()
-    return gesture_label_int, gesture_detected
-
-
-def get_frame_names(frames):
-    nclips = 1
-    is_val = False
-    step_size = 2
-    clip_size = 18
-    frame_names = frames
-    num_frames = len(frames)
-
-    if nclips > -1:
-        num_frames_necessary = clip_size * nclips * step_size
-    else:
-        num_frames_necessary = num_frames
-
-    offset = 0
-    if num_frames_necessary > num_frames:
-        frame_names += [frame_names[-1]] * (num_frames_necessary - num_frames)
-    elif num_frames_necessary < num_frames:
-        diff = num_frames - num_frames_necessary
-        if not is_val:
-            offset = np.random.randint(0, diff)
-    frame_names = frame_names[offset : num_frames_necessary + offset : step_size]
-    return frame_names
-
-
-def load_model(config_path):
-    config = load_config(config_path)
-    model = ConvColumn(8)
-    if os.path.isfile(config["checkpoint"]):
-        checkpoint = torch.load(config["checkpoint"], map_location="cpu")
-        new_state_dict = OrderedDict()
-
-        for k, v in checkpoint.items():
-            if k == "state_dict":
-                for j, val in v.items():
-                    name = j[7:]
-                    new_state_dict[name] = val
-                model.load_state_dict(new_state_dict)
-                break
-        print("Loaded checkpoint")
-    else:
-        print("No checkpoint found at '{}'".format(config["checkpoint"]))
-    return model
-
-
 @app.route("/page_content")
 def page_content():
     page = current_page["page"]
@@ -115,70 +40,24 @@ def page_content():
     return render_template_string(page_html)
 
 
-def process_video_stream(model, device, transform, arduino_port):
-    width = 176
-    height = 100
+def process_video_stream(arduino_port):
     idx = 0
     n = 0
-    gesture_buffer = []
-    frames = np.empty((0, height, width, 3))
-    gesture_label_int = None
     start_time = time.time()
 
     while True:
-        raw_frame = capture_image()
-        raw_frame = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2RGB)
-        raw_frame = cv2.resize(raw_frame, (176, 100))
-        frames = np.append(frames, [raw_frame], axis=0)
-        n += 1
-        if n == 37:
-            imgs = []
-            frames = get_frame_names(frames)
-            for frame in frames:
-                frame = Image.fromarray((frame * 255).astype(np.uint8))
-                frame = transform(frame)
-                imgs.append(torch.unsqueeze(frame, 0))
-
-            data = torch.cat(imgs)
-            data = data.permute(1, 0, 2, 3)
-            data = data[None, :, :, :, :]
-            target = torch.tensor([2])
-            data = data.to(device)
-
-            print("predicting...")
-            output = model(data)
-            gesture_label_int, gesture_detected = accuracy(
-                output.detach(), target.detach().cpu(), topk=(1,)
-            )
-            gesture_buffer.append(gesture_label_int)
-            print(gesture_buffer)
-            gesture_buffer = gesture_buffer[-10:]
-            no_action_count = gesture_buffer.count(0)
-            # print("no action count:", no_action_count)
-            n = 0
-            frames = np.empty((0, 100, 176, 3))
-            if no_action_count > DELAY_COUNT:
-                check_time = time.time()
-                time_delta = check_time - start_time
-                gesture_buffer.clear()
-                print("no action seconds elapsed:", time_delta)
-                if time_delta > 20:
-                    print("Elapsed 20 seconds of inactivity")
-                    idx = 0
-            if gesture_label_int == 1:
-                start_time = time.time()
-                idx += 1
-            elif gesture_label_int == 2:
-                start_time = time.time()
-                idx -= 1
-
+        time.sleep(0.5)
+        check_time = time.time()
+        time_delta = check_time - start_time
+        if time_delta > SWITCHING_DELAY:
+            print("Elapsed 20 seconds")
+            start_time = time.time()
+            idx += 1
             idx = idx % NUM_PAGES
             page = pages[idx]
             current_page["page"] = page
             if arduino_port:
                 communicate_with_arduino(arduino_port, idx)
-
-            # Emit the page change event to all connected clients
             socketio.emit("page_change", {"page": page})
 
 
@@ -216,21 +95,9 @@ def handle_connect():
 
 if __name__ == "__main__":
     arduino_port = find_arduino_port()
-    model = load_model("config.json")
-    model.eval()
-    model = torch.quantization.quantize_dynamic(model, {nn.Linear}, dtype=torch.qint8)
-    # model = torch.jit.script(model)
 
-    device = torch.device("cpu")
-    transform = Compose(
-        [
-            CenterCrop(84),
-            ToTensor(),
-            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
     video_thread = Thread(
-        target=process_video_stream, args=(model, device, transform, arduino_port)
+        target=process_video_stream, args=(arduino_port,)
     )
     video_thread.daemon = True
     video_thread.start()
@@ -238,4 +105,6 @@ if __name__ == "__main__":
     # Print the IP address
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
+    print(f"Server running on {ip_address}:5001")
+
     socketio.run(app, host="0.0.0.0", port=5001, debug=True)
--- utils.py
+++ utils.py
@@ -2,49 +2,9 @@ import os
 import time
 import json
 import serial
-import subprocess
 import numpy as np
-from PIL import Image
-import torch.nn as nn
 from os.path import join
 import serial.tools.list_ports
-
-
-class ConvColumn(nn.Module):
-
-    def __init__(self, num_classes):
-        super(ConvColumn, self).__init__()
-        self.conv_layer1 = self._make_conv_layer(3, 64, (1, 2, 2), (1, 2, 2))
-        self.conv_layer2 = self._make_conv_layer(64, 128, (2, 2, 2), (2, 2, 2))
-        self.conv_layer3 = self._make_conv_layer(128, 256, (2, 2, 2), (2, 2, 2))
-        self.conv_layer4 = self._make_conv_layer(256, 256, (2, 2, 2), (2, 2, 2))
-
-        self.fc5 = nn.Linear(12800, 512)
-        self.fc5_act = nn.ELU()
-        self.fc6 = nn.Linear(512, num_classes)
-
-    def _make_conv_layer(self, in_c, out_c, pool_size, stride):
-        conv_layer = nn.Sequential(
-            nn.Conv3d(in_c, out_c, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm3d(out_c),
-            nn.ELU(),
-            nn.MaxPool3d(pool_size, stride=stride, padding=0),
-        )
-        return conv_layer
-
-    def forward(self, x):
-        x = self.conv_layer1(x)
-        x = self.conv_layer2(x)
-        x = self.conv_layer3(x)
-        x = self.conv_layer4(x)
-
-        x = x.view(x.size(0), -1)
-
-        x = self.fc5(x)
-        x = self.fc5_act(x)
-
-        x = self.fc6(x)
-        return x
 
 
 def read_html_file(file_path):
@@ -61,31 +21,6 @@ def load_config(config_name, CONFIG_PATH="./config"):
     with open(join(CONFIG_PATH, config_name)) as file:
         config = json.load(file)
     return config
-
-
-def capture_image():
-    # Define the image filename
-    image_filename = "temp.jpg"
-
-    with open(os.devnull, "w") as devnull:
-        subprocess.run(
-            [
-                "fswebcam",
-                "--no-banner",
-                "-r",
-                "176x100",
-                "--jpeg",
-                "50",
-                image_filename,
-            ],
-            stdout=devnull,
-            stderr=devnull,
-        )
-
-    # Open the image using PIL and convert to a NumPy array
-    with Image.open(image_filename) as img:
-        image_array = np.array(img)
-    return image_array
 
 
 def list_ports():
